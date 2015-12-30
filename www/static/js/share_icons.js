4cec745effd17ab39e6ee6f82493b8952165879f
Ziyad Soobhan <zsoobhan@gmail.com>
2015-12-30 13:03:17 +0000

    Adds ga analytics id and Facebook ID

--- www/static/js/share_icons.js
+++ www/static/js/share_icons.js
@@ -1,7 +1,6 @@
 var share = window.share || {};
 
 share.facebookShareButton = {
-
     listenForEvent: function () {
         'use_strict';
         $("#fb-share-link-js").click(function (e){
@@ -10,14 +9,10 @@ share.facebookShareButton = {
             share.facebookShareButton.openShareWindow(); 
         });
     },
-
-
     pushGAEvent: function(slug){
         'use_strict';
         ga('send', 'event', 'blog', 'click', slug);
     },
-
-
     openShareWindow: function(){
         'use_strict';
         window.open('https://www.facebook.com/sharer/sharer.php?u='
